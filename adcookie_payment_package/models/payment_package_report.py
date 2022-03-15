a476ab1ca6be7b30cdf4c3be12015f676915dfde
Patryk Prokurat <pp@myodoo.pl>
2022-03-15 16:06:43 +0100

    quick check

--- adcookie_payment_package/models/payment_package_report.py
+++ adcookie_payment_package/models/payment_package_report.py
@@ -6,20 +6,33 @@ import importlib
 class PaymentPackageReport(models.AbstractModel):
     _name = 'report.adcookie_payment_package.payment_package_report'
 
-    def render_html(self, data=None):
-        report_obj = self.env['report']
+    # def render_html(self, data=None):
+    #     report_obj = self.env['report']
+    #
+    #     report = report_obj._get_report_from_name('adcookie_payment_package.payment_package_report')
+    #
+    #     docargs = {
+    #         'doc_ids': self._ids,
+    #         'doc_model': report.model,
+    #         'docs': self,
+    #         'set_windows_coding': self._set_windows_coding,
+    #         'set_default_coding': self._set_default_coding
+    #     }
+    #
+    #     return report_obj.render('adcookie_payment_package.payment_package_report', docargs)
 
-        report = report_obj._get_report_from_name('adcookie_payment_package.payment_package_report')
+    def _get_report_values(self, docids, data=None):
+        model = self.env.context.get('active_model')
+        docs = self.env[model].browse(self.env.context.get('active_id'))
 
-        docargs = {
-            'doc_ids': self._ids,
-            'doc_model': report.model,
-            'docs': self,
+        return {
+            'doc_ids': docids,
+            'doc_model': model,
+            'data': data,
+            'docs': docs,
             'set_windows_coding': self._set_windows_coding,
             'set_default_coding': self._set_default_coding
         }
-
-        return report_obj.render('adcookie_payment_package.payment_package_report', docargs)
 
     def _set_windows_coding(self):
         importlib.reload(sys)
