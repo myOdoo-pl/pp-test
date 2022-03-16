e5339a8d96bcaa78760c5e932cf540ba8716be5a
Patryk Prokurat <pp@myodoo.pl>
2022-03-16 12:12:49 +0100

    quick check

---
+++ adcookie_invoice_pivot_view/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "AdCookie Invoice Pivot View",
+    'summary': 'Invoice pivot view modification for AdCookie.',
+    'author': 'MyOdoo.pl',
+    'website': 'https://myodoo.pl',
+    'category': 'Tools',
+    'version': '[V14]_0.1',
+    'depends': [
+        'account',
+        'partner_company_group'
+    ],
+    'data': [
+
+    ],
+    'installable': True
+}
--- adcookie_invoice_pivot_view/report/account_invoice_report.py
+++ adcookie_invoice_pivot_view/report/account_invoice_report.py
@@ -4,6 +4,8 @@ from odoo import models, fields, api
 class AccountInvoiceReport(models.Model):
     _inherit = "account.invoice.report"
 
+    company_group_id = fields.Many2one('res.partner', string='Company Group', readonly=True)
+
     @api.model
     def _select(self):
         return super(AccountInvoiceReport, self)._select() + ", move.company_group_id as company_group_id"
