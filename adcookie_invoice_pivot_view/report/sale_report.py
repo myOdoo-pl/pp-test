6ea408f75ff42c34f0ffe0cd475c24e39904579d
Patryk Prokurat <pp@myodoo.pl>
2022-03-17 15:39:05 +0100

    quick check

--- adcookie_invoice_pivot_view/report/sale_report.py
+++ adcookie_invoice_pivot_view/report/sale_report.py
@@ -7,4 +7,4 @@ class SaleReport(models.Model):
     company_group_id = fields.Many2one('res.partner', string='Company Group', readonly=True)
 
     def _select_sale(self, fields=None):
-        return super(SaleReport, self, fields)._select_sale() + ", s.company_group_id as company_group_id"
+        return super(SaleReport, self)._select_sale() + ", s.company_group_id as company_group_id"
